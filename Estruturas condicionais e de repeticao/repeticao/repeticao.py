edcd8eef22d817b7a8d160f0a802374490bb5e4e
genilson <genilson_junior14@outlook.com>
2025-05-31 11:06:05 -0300

    while

--- Estruturas condicionais e de repeticao/repeticao/repeticao.py
+++ Estruturas condicionais e de repeticao/repeticao/repeticao.py
@@ -13,3 +13,16 @@ while a > 0:
     a-= 1
     print(a, end=" ") #para não pular linha
 print("\nFim do programa")
+
+print("Programa 3: for com range")
+#Range
+for numero in range(0, 11):
+    print(numero, end=" ") #para não pular linha
+
+# list(range(4)) # [0, 1, 2, 3]
+
+print()#pula uma linha
+
+#exivindo a tabuada
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
---
+++ Estruturas condicionais e de repeticao/repeticao/while.py
@@ -0,0 +1,24 @@
+opcao = -1
+
+def menu():
+    print("Opcoes")
+    print("[1] opcao 1")
+    print("[2] opcao 2")
+    print("[3] opcao 3")
+    print("[0] Sair")
+
+
+while opcao != 0:
+    menu()
+    opcao = int(input("\nInforme uma opcao (0 para sair): "))
+
+    if opcao == 1:
+        print("Opcao 1 selecionada")
+    elif opcao == 2:
+        print("Opcao 2 selecionada")
+    elif opcao == 3:
+        print("Opcao 3 selecionada")
+    elif opcao == 0:
+        print("Saindo do programa")
+    else:
+        print("Opcao invalida, tente novamente")
