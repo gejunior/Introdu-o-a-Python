bcdaaad9b34189db2fe8356fe38597eb9f405f2c
genilson <genilson_junior14@outlook.com>
2025-05-30 23:48:22 -0300

    adicionando comparadores

---
+++ comparadores.py
@@ -0,0 +1,12 @@
+saldo = 100
+saque = 25
+print("Saldo: ", saldo)
+print("Saque: ", saque)
+print("Saldo maior igual a saque? ", saldo >= saque)
+print("Saldo menor igual a saque? ", saldo <= saque)
+print("Saldo maior que saque? ", saldo > saque)
+print("Saldo menor que saque? ", saldo < saque)
+print("Saldo igual a saque? ", saldo == saque)
+print("Saldo diferente de saque? ", saldo != saque)
+
+
