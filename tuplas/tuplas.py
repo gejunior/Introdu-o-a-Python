9534ace3aa85bc009d1cc612237c5ab9f6114a1e
genilson <genilson_junior14@outlook.com>
2025-06-06 15:22:59 -0300

    adicionando tuplas

--- tuplas/tuplas.py
+++ tuplas/tuplas.py
@@ -1,8 +1,19 @@
 #smp quando tgratar de tuplas, smp vai ter virgula no final
 # Tuplas são imutáveis, ou seja, não podem ser alteradas após a criação
+
+#tuplas podem armazenar diferentes tipos de dados
+# Tuplas são definidas com parênteses e podem conter diferentes tipos de dados
+# Exemplo de tuplas
+tupla_vazia = ()
+tupla_inteiros = (1, 2, 3, 4, 5)
+tupla_mista = (1, "Python", 3.14, True)
+#posso usar a tupla para garantir que os dados não serão alterados
 frutas = ("laranja", "maçã", "banana", "uva",)
 
 letras = tuple("python")
 numeros = tuple([1, 2, 3, 4])
 
 pais = ("Brasil",)
+
+#Fatiamento
+#msm lógica que listas
