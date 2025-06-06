24069bf68042c9adaa401161ac0c759964ccd9e8
genilson <genilson_junior14@outlook.com>
2025-06-06 15:17:25 -0300

    atualizando lista e tuplas

--- listas/lista.py
+++ listas/lista.py
@@ -48,3 +48,33 @@ numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]  # Compreensão de lista para filtrar números pares
 # Adiciona o número par à lista pares
 print("Números pares:", pares)  # Imprime a lista de números pares
+
+print("printando lista antes de limpar", lista)
+lista.clear()  # Limpa a lista, removendo todos os elementos
+l2 = lista.copy()  # Cria uma cópia da lista
+print("printando depois de limpar", lista) 
+
+# print(id(lista))  # Imprime o ID da lista (endereço de memória)
+# print(id(frutas))  # Imprime o ID da lista frutas (endereço de memória)
+
+# l2[0] = 2
+print(l2)
+
+# use extend para adicionar elementos de outra lista
+l2.extend([1, 2, 3])  # Adiciona os elementos da lista [1, 2, 3] à lista l2
+print(l2)  # Imprime a lista l2 após adicionar os elementos 
+
+linguagens = ["python", "java", "c++", "javascript"]
+
+linguagens.index("python")  # Retorna o índice do elemento "python" na lista linguagens
+print(linguagens.index("python"))  # Imprime o índice/posição do elemento "python" na lista linguagens
+linguagens.append("c#")  # Adiciona o elemento "c#" ao final da lista linguagens
+print(linguagens)  # Imprime a lista linguagens após adicionar o elemento "c#"
+
+linguagens.pop()  # Remove e retorna o último elemento da lista linguagens
+linguagens.pop(0)  # Remove e retorna o elemento no índice 0 da lista linguagens
+linguagens.remove("java")  # Remove o elemento "java" da lista linguagens
+linguagens.reverse()  # Inverte a ordem dos elementos na lista linguagens
+linguagens.sort()  # Ordena os elementos da lista linguagens em ordem crescente
+len(linguagens)  # Retorna o número de elementos(tamanho) na lista linguagens
+sorted(linguagens)  # Retorna uma nova lista ordenada, sem modificar a lista original
---
+++ tuplas/tuplas.py
@@ -0,0 +1,8 @@
+#smp quando tgratar de tuplas, smp vai ter virgula no final
+# Tuplas são imutáveis, ou seja, não podem ser alteradas após a criação
+frutas = ("laranja", "maçã", "banana", "uva",)
+
+letras = tuple("python")
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
