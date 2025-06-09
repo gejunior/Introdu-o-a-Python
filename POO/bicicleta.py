5d9c34d8126c4acf3a157b0097beeadba1218c97
genilson <genilson_junior14@outlook.com>
2025-06-08 22:28:11 -0300

    exemplo de poo

--- POO/bicicleta.py
+++ POO/bicicleta.py
@@ -14,11 +14,18 @@ class bicicleta:
         print("parô!!!")
 
     def correr(self):
-        print("vruuuuum!")
+        print("vruuuuum!")        
+
+    def __str__(self):#metodo ñ precisa ficar alterando
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' 
+                                                        for chave, valor in self.__dict__.items()])}"
+    
 
 
 b1 = bicicleta("vermelha", "caloy", 2022, 600) #instancia de bicicleta
-b1.businar()
-b1.correr()
-b1.parar()
-print(b1.cor, b1.modelo, b1.ano, b1.valor)
+# b1.businar()
+# b1.correr()
+# b1.parar()
+# print(b1.cor, b1.modelo, b1.ano, b1.valor)
+
+print(b1.__str__())
